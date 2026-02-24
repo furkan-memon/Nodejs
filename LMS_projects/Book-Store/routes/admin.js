f2734497c09ca9d9655d1299cb78fd02f136a005
furkan-memon <furkanmemon860@gmail.com>
2026-02-24 12:43:37 +0530

    '2-24-26'

--- LMS_projects/Book-Store/routes/admin.js
+++ LMS_projects/Book-Store/routes/admin.js
@@ -34,9 +34,48 @@ router.post("/upload", upload.single("image"), async (req, res) => {
 });
 
 
+
 router.get("/product", async (req, res) => {
   const product = await Product.find();
   res.render("product", { product });
 });
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+
+    res.render('edit', { product });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+router.post('/update/:id', upload.single('image'), async (req, res) => {
+  try {
+
+    const { name, authorname, price } = req.body;
+
+    const updateData = { name, authorname, price };
+
+    if (req.file) {
+      updateData.image = req.file.buffer;
+    }
+
+    await Product.findByIdAndUpdate(req.params.id, updateData);
+
+    res.redirect('/admin/product');
+
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+router.get('/delete/:id', async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.redirect('/admin/product');
+});
 module.exports = router;
