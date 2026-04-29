46754ba880e11dc16021f2225c70a688923c9426
furkan_memon <furkanmemon860@gmail.com>
2026-04-29 16:15:40 +0530

    Delete exam/javascript-master directory

--- exam/javascript-master/e-com website/config/api.js
+++
@@ -1,9 +0,0 @@
-
-const BaseUrl=`http://localhost:3000`
-
-export const apiUrl={
-    users: `${BaseUrl}/user`,
-    products:`${BaseUrl}/products`,
-    carts:`${BaseUrl}/carts`
-
-}
--- exam/javascript-master/e-com website/javascript/index.js
+++
@@ -1,8 +0,0 @@
-import Navbar from "../components/navbar.js";
-import { isLoggedIn } from "../utils/helper.js";
-document.getElementById("navbar").innerHTML=Navbar();
-let  logoutBtn = document.getElementById("logout-btn");
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", handleLogout);
-}
-isLoggedIn()
